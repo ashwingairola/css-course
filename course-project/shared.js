d8adc949f75274e4507a98e40274cf3a8b0b892b
Ashwin <ashwingairola999@gmail.com>
2020-09-07 23:03:59 +0530

    138. Manipulating Element Classes

--- course-project/shared.js
+++ course-project/shared.js
@@ -8,25 +8,36 @@ const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
 
 const closeModal = () => {
-	modal.style.display = 'none';
-	backdrop.style.display = 'none';
+	// modal.style.display = 'none';
+	// backdrop.style.display = 'none';
+
+	modal.classList.remove('open');
+	backdrop.classList.remove('open');
 };
 
 selectPlanButtons.forEach((button) => {
 	button.addEventListener('click', () => {
-		backdrop.style.display = 'block';
-		modal.style.display = 'block';
+		// backdrop.style.display = 'block';
+		// modal.style.display = 'block';
+
+		modal.classList.add('open');
+		backdrop.classList.add('open');
 	});
 });
 
 backdrop.addEventListener('click', () => {
-	mobileNav.style.display = 'none';
+	// mobileNav.style.display = 'none';
+
+	mobileNav.classList.remove('open');
 	closeModal();
 });
 
 modalNoButton.addEventListener('click', closeModal);
 
 toggleButton.addEventListener('click', () => {
-	mobileNav.style.display = 'block';
-	backdrop.style.display = 'block';
+	// mobileNav.style.display = 'block';
+	// backdrop.style.display = 'block';
+
+	mobileNav.classList.add('open');
+	backdrop.classList.add('open');
 });
